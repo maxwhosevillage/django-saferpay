6556e48b0bee53ac2368af194bf640581cb42a0a
Max Wessendorf <m.wessendorf@ukd.de>
2025-05-19 10:36:03 +0200

    urls --> re_path

--- saferpay/urls.py
+++ saferpay/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^notify/(?P<notify_token>.+)/$', views.NotifyView.as_view(), name='saferpay-notify'),
+    re_path(r'^notify/(?P<notify_token>.+)/$', views.NotifyView.as_view(), name='saferpay-notify'),
 ]
